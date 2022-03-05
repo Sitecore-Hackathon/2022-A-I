ee7cb743e697a06904de0026a3e12b74ac19ad36
rbatallasOshyn <rbatallas@oshyn.com>
2022-03-05 07:50:02 -0500

    Evaluation Templates

---
+++ src/Feature/Evaluation/Mvp.Feature.Evaluation.Rendering/Extensions/RenderingEngineOptionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Mvp.Feature.Evaluation.Rendering.Model;
+using Sitecore.AspNet.RenderingEngine.Configuration;
+using Sitecore.AspNet.RenderingEngine.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mvp.Feature.Evaluation.Rendering.Extensions
+{
+    public static class RenderingEngineOptionsExtensions
+    {
+        public static RenderingEngineOptions AddFeatureBasicContent(this RenderingEngineOptions options)
+        {
+            options.AddModelBoundView<EvaluationModel>("Evaluation");
+
+            return options;
+        }
+    }
+}
---
+++ src/Feature/Evaluation/Mvp.Feature.Evaluation.Rendering/Model/EvaluationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Mvp.Feature.Evaluation.Rendering.Model
+{
+    public class EvaluationModel
+    {
+        public NameValueCollection Metric { get; set; }
+    }
+}
